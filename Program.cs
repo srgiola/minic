1c21600860bd5b35c520e5981e135525e29c9a4e
Sergio Lara <srgiokun@gmail.com>
2020-08-14 21:32:39 -0600

    Comentario Inicial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace minic
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Ingrese la dirección de su archivo");
+			string file = Console.ReadLine();
+			//string file = "C:\\Users\\srgio\\Desktop\\Ejemplo.frag";
+			//string file = "";
+			if (!File.Exists(file))
+				Console.WriteLine("El archivo no existe");
+			else
+			{
+				// [0] = file name     [1] = file extencion
+				string[] fileParts = Path.GetFileName(file).Split('.');
+				if (!(fileParts[1] == "frag"))
+					Console.WriteLine("El archivo debe tener una extención valida");
+			}
+		}
+	}
+}
